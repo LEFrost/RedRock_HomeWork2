298e611c0db2c570739fe186425dfc8928ecf1d9
彭建 <pengjian@live.cn>
2015-12-05 00:32:46 +0800

    作业

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            func2();
+
+        }
+        public static async Task<string> func()
+        {
+            return await Task.Run(() =>
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Thread.Sleep(2000);
+                    Console.Write('$');
+                }
+                return "";
+
+            });
+        }
+        public static string func2()
+        {
+            func();
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine('@');
+            }
+
+            return "";
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(p =>
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.Write('$');
+                    Thread.Sleep(2000);
+                }
+
+
+            });
+            t.Start();
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine('@');
+                Thread.Sleep(1000);
+            }
+            Console.ReadKey();
+        }
+    }
+}
